6d653ddb64be4ebf6d828f62e2217b45ecd706d0
Pork Lin <devilheaven57@gmail.com>
2015-11-30 16:12:54 +0800

    ccccccccccccccccc

--- DemoWeb/src/controller/LoginServlet.java
+++ DemoWeb/src/controller/LoginServlet.java
@@ -20,7 +20,7 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("gggggg");
+		System.out.println("ggggggcc");
 
 
 
